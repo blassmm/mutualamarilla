94c7ba498589de38c7962a5cffe4772d93891fd5
moragentil <moragentil@gmail.com>
2025-11-06 13:18:07 -0300

    responsive

--- components/ayuda-economica/hero.tsx
+++ components/ayuda-economica/hero.tsx
@@ -4,14 +4,13 @@ import Image from "next/image"
 
 export function Hero() {
     return (
-        <section className="relative bg-white h-[600px]">
+        <section className="relative bg-white h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] overflow-hidden">
         <div className="relative h-full w-full z-0">
           <Image
             src="/images/ayuda-economica-banner.jpg"
             alt="Manos unidas en equipo"
             fill
-            className="object-cover"
-            style={{ objectPosition: 'center 40%' }}
+            className="object-cover object-center"
             priority
           />
         </div>
